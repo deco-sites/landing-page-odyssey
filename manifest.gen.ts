b8e574ab2bf78c99943ecf37dc0369dfeee2f409
Tiago Gimenes <gimenes@deco.cx>
2024-04-03 12:21:17 +0000

    Release Staging environment with 3 changes made by Viktor Marinho Pereira, Tiago Gimenes

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,49 +2,49 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/AOSInitializer.tsx";
-import * as $$$$$$1 from "./sections/BlogPosts.tsx";
-import * as $$$$$$2 from "./sections/Faq.tsx";
-import * as $$$$$$3 from "./sections/Footer.tsx";
-import * as $$$$$$4 from "./sections/Gallery.tsx";
-import * as $$$$$$5 from "./sections/Hal.tsx";
-import * as $$$$$$6 from "./sections/Header.tsx";
-import * as $$$$$$14 from "./sections/hero-1.tsx";
-import * as $$$$$$15 from "./sections/hero-2.tsx";
-import * as $$$$$$7 from "./sections/Hero.tsx";
-import * as $$$$$$16 from "./sections/hero2.tsx";
-import * as $$$$$$8 from "./sections/ImageWithParagraph.tsx";
-import * as $$$$$$9 from "./sections/Intro.tsx";
-import * as $$$$$$10 from "./sections/Logos.tsx";
-import * as $$$$$$11 from "./sections/Stats.tsx";
-import * as $$$$$$12 from "./sections/Testimonials.tsx";
-import * as $$$$$$13 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$0 from "./sections/AOSInitializer.tsx";
+import * as $$$$$$$1 from "./sections/BlogPosts.tsx";
+import * as $$$$$$$2 from "./sections/Faq.tsx";
+import * as $$$$$$$3 from "./sections/Footer.tsx";
+import * as $$$$$$$4 from "./sections/Gallery.tsx";
+import * as $$$$$$$5 from "./sections/Hal.tsx";
+import * as $$$$$$$6 from "./sections/Header.tsx";
+import * as $$$$$$$14 from "./sections/hero-1.tsx";
+import * as $$$$$$$15 from "./sections/hero-2.tsx";
+import * as $$$$$$$7 from "./sections/Hero.tsx";
+import * as $$$$$$$16 from "./sections/hero2.tsx";
+import * as $$$$$$$8 from "./sections/ImageWithParagraph.tsx";
+import * as $$$$$$$9 from "./sections/Intro.tsx";
+import * as $$$$$$$10 from "./sections/Logos.tsx";
+import * as $$$$$$$11 from "./sections/Stats.tsx";
+import * as $$$$$$$12 from "./sections/Testimonials.tsx";
+import * as $$$$$$$13 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "sections": {
-    "site/sections/AOSInitializer.tsx": $$$$$$0,
-    "site/sections/BlogPosts.tsx": $$$$$$1,
-    "site/sections/Faq.tsx": $$$$$$2,
-    "site/sections/Footer.tsx": $$$$$$3,
-    "site/sections/Gallery.tsx": $$$$$$4,
-    "site/sections/Hal.tsx": $$$$$$5,
-    "site/sections/Header.tsx": $$$$$$6,
-    "site/sections/hero-1.tsx": $$$$$$14,
-    "site/sections/hero-2.tsx": $$$$$$15,
-    "site/sections/Hero.tsx": $$$$$$7,
-    "site/sections/hero2.tsx": $$$$$$16,
-    "site/sections/ImageWithParagraph.tsx": $$$$$$8,
-    "site/sections/Intro.tsx": $$$$$$9,
-    "site/sections/Logos.tsx": $$$$$$10,
-    "site/sections/Stats.tsx": $$$$$$11,
-    "site/sections/Testimonials.tsx": $$$$$$12,
-    "site/sections/Theme/Theme.tsx": $$$$$$13,
+    "site/sections/AOSInitializer.tsx": $$$$$$$0,
+    "site/sections/BlogPosts.tsx": $$$$$$$1,
+    "site/sections/Faq.tsx": $$$$$$$2,
+    "site/sections/Footer.tsx": $$$$$$$3,
+    "site/sections/Gallery.tsx": $$$$$$$4,
+    "site/sections/Hal.tsx": $$$$$$$5,
+    "site/sections/Header.tsx": $$$$$$$6,
+    "site/sections/hero-1.tsx": $$$$$$$14,
+    "site/sections/hero-2.tsx": $$$$$$$15,
+    "site/sections/Hero.tsx": $$$$$$$7,
+    "site/sections/hero2.tsx": $$$$$$$16,
+    "site/sections/ImageWithParagraph.tsx": $$$$$$$8,
+    "site/sections/Intro.tsx": $$$$$$$9,
+    "site/sections/Logos.tsx": $$$$$$$10,
+    "site/sections/Stats.tsx": $$$$$$$11,
+    "site/sections/Testimonials.tsx": $$$$$$$12,
+    "site/sections/Theme/Theme.tsx": $$$$$$$13,
   },
   "apps": {
-    "site/apps/decohub.ts": $$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$$0,
+    "site/apps/site.ts": $$$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
