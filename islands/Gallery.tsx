8fd8986635d59ad7b640a3cf82062469843f01e5
João Pesce <jpesce@gmail.com>
2024-04-03 22:03:33 -0300

    feat: add close button to gallery

--- islands/Gallery.tsx
+++ islands/Gallery.tsx
@@ -22,6 +22,22 @@ function GalleryModal({ imageName, id }: { imageName: string; id: string }) {
       <div className="modal-box rounded-none w-screen max-w-none max-h-none p-0 relative">
         <img src={`/${imageName}`} />
       </div>
+      <button class="btn btn-circle btn-outline fixed top-6 right-6 text-stone-100 opacity-75">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
       <div class="modal-backdrop bg-black" />
       <form
         method="dialog"
