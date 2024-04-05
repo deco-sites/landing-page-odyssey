8293048eb85b281f1040b9c90509ffa9b3d2eb25
João Pesce <jpesce@gmail.com>
2024-04-04 23:22:56 -0300

    refactor: use Image component for gallery images

--- islands/Gallery.tsx
+++ islands/Gallery.tsx
@@ -1,3 +1,5 @@
+import Image from "apps/website/components/Image.tsx";
+
 function shuffleArray(array: Array<unknown>) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -6,21 +8,21 @@ function shuffleArray(array: Array<unknown>) {
 }
 
 const IMAGES = [
-  "still-2.jpg",
-  "still-23.jpg",
-  "still-4.jpg",
-  "still-5.jpg",
-  "still-21.jpg",
-  "still-8.jpg",
-  "still-9.jpg",
-  "still-10.jpg",
-  "still-22.jpg",
-  "still-12.jpg",
-  "still-19.jpg",
-  "still-14.jpg",
-  "still-15.jpg",
-  "still-20.jpg",
-  "still-17.jpg",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/6ecd94d4-3a9a-47c5-a926-431342f563ab",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/f84af164-2349-4cce-9d05-a5859ed3e233",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/53cbfeec-752a-48e0-9924-af99229e41f4",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/6f587430-5207-4d84-ab5e-91544e04a6e4",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/c33c0a1d-aafd-43e7-afc0-5573b56b5328",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/513b0a23-973f-4fee-96f9-ef134ac75d9d",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/7b8d12b5-b28b-4579-93a3-e2075b52ac3e",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/43c23f59-b9d3-4005-9e4e-8966837a6d9b",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/56d97b2b-9e4f-4675-9a61-42e39946102c",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/4234d3c4-07bd-4868-b257-0667d1c9305d",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/354c2d94-9e58-480e-81f6-eb817a66a60f",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/e4d38c0a-44f4-4663-a523-b7a2056457b8",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/15c4a7ad-4d12-4d5a-aec5-2a658791a3e8",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/0b348cd7-bde8-4fa9-8f4e-aefce8c4833d",
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4946/e61f5d81-c8d6-4eb7-8fa5-d8d62711143f",
 ];
 
 shuffleArray(IMAGES);
@@ -75,14 +77,15 @@ function GalleryItem(
           (document?.getElementById(modalId) as HTMLFormElement)?.showModal()}
         class="hover:scale-105 hover:drop-shadow-2xl transition duration-500 "
       >
-        <img
+        <Image
           data-aos="fade-up-discrete"
           data-aos-anchor="#gallery"
           data-aos-delay={index * 150}
           data-aos-offset={400}
-          src={`/${imageName}`}
-        >
-        </img>
+          src={`${imageName}`}
+          width={371}
+          height={170}
+        />
       </button>
     </>
   );
